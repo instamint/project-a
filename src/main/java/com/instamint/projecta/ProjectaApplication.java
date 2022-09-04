bb39a73ec4c9c1d1bff45b812d9bda3b5c40b3a0
Aaron <530816249@qq.com>
2022-09-04 19:47:35 +0800

    Enable port number for projecta

--- src/main/java/com/instamint/projecta/ProjectaApplication.java
+++ src/main/java/com/instamint/projecta/ProjectaApplication.java
@@ -1,6 +1,5 @@
 package com.instamint.projecta;
 
-import com.instamint.sharedatamodels.models.AssetType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
