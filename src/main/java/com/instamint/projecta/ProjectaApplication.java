feb7a41a57ad605a3617822ea0af1654b0ab9ccb
Aaron <530816249@qq.com>
2022-09-04 15:08:56 +0800

    Set up the shared-data-model for the projecta.

--- src/main/java/com/instamint/projecta/ProjectaApplication.java
+++ src/main/java/com/instamint/projecta/ProjectaApplication.java
@@ -1,5 +1,6 @@
 package com.instamint.projecta;
 
+import com.instamint.sharedatamodels.models.AssetType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
